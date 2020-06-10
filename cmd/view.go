a87feab7c262de4eec4ac4540c43bd97398e6021
Alexsandre <alexsandrer@gmail.com>
2020-06-09 22:15:20 -0300

    Reorganize project structure and modules

--- cmd/view.go
+++ cmd/view.go
@@ -24,8 +24,13 @@ import (
 // viewCmd represents the view command
 var viewCmd = &cobra.Command{
 	Use:   "view",
-	Short: "short",
-	Long:  `long`,
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("view called")
 	},
@@ -43,8 +48,4 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// viewCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-func runServer() {
-
 }
